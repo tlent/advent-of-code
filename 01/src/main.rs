4f95b2830caa5d4efe6ef432bb0305b6d964d7b9
Thomas Lent <tcl293@gmail.com>
2022-12-02 10:42:54 -0500

    Simplify day 1 solution

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,39 +1,30 @@
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    let inventories = parse_input(&INPUT);
-    println!("{}", max_calories(&inventories));
-    println!("{}", three_max_sum(&inventories));
+    let calorie_totals = parse_input(&INPUT);
+    let max_calorie_total = calorie_totals.iter().max().unwrap();
+    println!("{}", max_calorie_total);
+    let three_largest_sum: u32 = three_largest(&calorie_totals).iter().sum();
+    println!("{}", three_largest_sum);
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u32>> {
-    let mut inventories = vec![];
-    let mut inventory = vec![];
+fn parse_input(input: &str) -> Vec<u32> {
+    let mut totals = vec![];
+    let mut total = 0;
     for line in input.lines() {
         if line.is_empty() {
-            inventories.push(inventory);
-            inventory = vec![];
+            totals.push(total);
+            total = 0;
         } else {
-            let value = line.parse().unwrap();
-            inventory.push(value);
+            let value: u32 = line.parse().unwrap();
+            total += value;
         }
     }
-    inventories
+    totals
 }
 
-fn max_calories(inventories: &[Vec<u32>]) -> u32 {
-    inventories
-        .iter()
-        .map(|inventory| inventory.iter().sum())
-        .max()
-        .unwrap()
-}
-
-fn three_max_sum(inventories: &[Vec<u32>]) -> u32 {
-    let mut sums: Vec<_> = inventories
-        .iter()
-        .map(|inventory| inventory.iter().sum())
-        .collect();
-    sums.sort_unstable();
-    sums[sums.len() - 3..].iter().sum()
+fn three_largest(values: &[u32]) -> [u32; 3] {
+    let mut values = values.to_vec();
+    values.sort_unstable();
+    values[values.len() - 3..].try_into().unwrap()
 }
