9d2dc37e760b3479a6ab47ca730f6a7f8e3457ac
Thomas Lent <tlent@pm.me>
2023-03-29 15:07:55 -0400

    Add benchmark for day 1

---
+++ 01/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn parse_input(input: &str) -> Vec<u32> {
+    input
+        .split("\n\n")
+        .map(|s| s.lines().map(|v| v.parse::<u32>().unwrap()).sum())
+        .collect()
+}
+
+pub fn part_one(sorted_group_sums: &[u32]) -> u32 {
+    *sorted_group_sums.last().unwrap()
+}
+
+pub fn part_two(sorted_group_sums: &[u32]) -> u32 {
+    sorted_group_sums.iter().rev().take(3).sum::<u32>()
+}
--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,24 +1,11 @@
 const INPUT: &str = include_str!("../input.txt");
 
+use advent_of_code_2022_day_01::{parse_input, part_one, part_two};
+
 fn main() {
     let mut group_sums = parse_input(&INPUT);
     group_sums.sort_unstable();
 
     println!("{}", part_one(&group_sums));
     println!("{}", part_two(&group_sums));
-}
-
-fn parse_input(input: &str) -> Vec<u32> {
-    input
-        .split("\n\n")
-        .map(|s| s.lines().map(|v| v.parse::<u32>().unwrap()).sum())
-        .collect()
-}
-
-fn part_one(sorted_group_sums: &[u32]) -> u32 {
-    *sorted_group_sums.last().unwrap()
-}
-
-fn part_two(sorted_group_sums: &[u32]) -> u32 {
-    sorted_group_sums.iter().rev().take(3).sum::<u32>()
 }
