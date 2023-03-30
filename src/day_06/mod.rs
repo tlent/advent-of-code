b39497abf139b54423396d795077cfcb639033e3
Thomas Lent <tlent@pm.me>
2023-03-30 00:06:23 -0400

    Optimize

--- src/day_06/mod.rs
+++ src/day_06/mod.rs
@@ -1,24 +1,27 @@
 pub const INPUT: &str = include_str!("input.txt");
 
 pub fn part_one(input: &str) -> usize {
-    find_unique_window_index(input.as_bytes(), 4).unwrap()
+    find_unique_window_index(input.trim().as_bytes(), 4).unwrap()
 }
 
 pub fn part_two(input: &str) -> usize {
-    find_unique_window_index(input.as_bytes(), 14).unwrap()
+    find_unique_window_index(input.trim().as_bytes(), 14).unwrap()
 }
 
 fn find_unique_window_index(bytes: &[u8], window_size: usize) -> Option<usize> {
-    let mut window_set = [0; 26];
-    for &byte in &bytes[..window_size] {
-        window_set[byte as usize - b'a' as usize] += 1;
-    }
-    for index in window_size..bytes.len() {
-        if window_set.iter().all(|&count| count <= 1) {
-            return Some(index);
+    let mut previous_index_by_letter = [None; 26];
+    let mut previous_index = vec![None; bytes.len()];
+    for index in 0..bytes.len() {
+        if index >= window_size {
+            let window_start = index - window_size;
+            let window = &previous_index[window_start..index];
+            if window.iter().all(|&i| i < Some(window_start)) {
+                return Some(index);
+            }
         }
-        window_set[bytes[index - window_size] as usize - b'a' as usize] -= 1;
-        window_set[bytes[index] as usize - b'a' as usize] += 1;
+        let index_by_letter = &mut previous_index_by_letter[bytes[index] as usize - b'a' as usize];
+        previous_index[index] = *index_by_letter;
+        *index_by_letter = Some(index);
     }
     None
 }
