86cf4a2e4728460499ada5979910759919a22428
Thomas Lent <tlent@pm.me>
2023-03-30 16:01:36 -0400

    Add day_03::parse_input for better performance

--- src/day_03/bench.rs
+++ src/day_03/bench.rs
@@ -2,14 +2,18 @@ use advent_of_code_2022::day_03::{self, INPUT};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let lines: Vec<_> = INPUT.lines().collect();
+    let (part_one_sets, part_two_sets) = day_03::parse_input(INPUT);
+
+    c.bench_function("day_03::parse_input", |b| {
+        b.iter(|| day_03::parse_input(black_box(INPUT)));
+    });
 
     c.bench_function("day_03::part_one", |b| {
-        b.iter(|| day_03::part_one(black_box(&lines)));
+        b.iter(|| day_03::part_one(black_box(&part_one_sets)));
     });
 
     c.bench_function("day_03::part_two", |b| {
-        b.iter(|| day_03::part_two(black_box(&lines)));
+        b.iter(|| day_03::part_two(black_box(&part_two_sets)));
     });
 }
 
--- src/day_03/bin.rs
+++ src/day_03/bin.rs
@@ -1,7 +1,7 @@
 use advent_of_code_2022::day_03::{self, INPUT};
 
 fn main() {
-    let lines: Vec<_> = INPUT.lines().collect();
-    println!("{}", day_03::part_one(&lines));
-    println!("{}", day_03::part_two(&lines));
+    let (part_one_sets, part_two_sets) = day_03::parse_input(INPUT);
+    println!("{}", day_03::part_one(&part_one_sets));
+    println!("{}", day_03::part_two(&part_two_sets));
 }
