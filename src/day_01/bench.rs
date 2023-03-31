26fe1f09613e81fb68c27aff5a879f6e646e15bb
Thomas Lent <tlent@pm.me>
2023-03-31 17:28:12 -0400

    Fix benchmark

--- src/day_01/bench.rs
+++ src/day_01/bench.rs
@@ -2,19 +2,18 @@ use advent_of_code_2022::day_01::{self, INPUT};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group_sums = day_01::parse_input(INPUT);
-    group_sums.sort_unstable();
+    let group_sums = day_01::parse_input(INPUT);
 
     c.bench_function("day_01::parse_input", |b| {
         b.iter(|| day_01::parse_input(black_box(INPUT)));
     });
 
-    c.bench_function("day_01::part_one", |b| {
-        b.iter(|| day_01::part_one(black_box(&group_sums)));
-    });
-
-    c.bench_function("day_01::part_two", |b| {
-        b.iter(|| day_01::part_two(black_box(&group_sums)));
+    c.bench_function("day_01::solve", |b| {
+        b.iter_batched(
+            || group_sums.clone(),
+            |group_sums| day_01::solve(black_box(group_sums)),
+            criterion::BatchSize::SmallInput,
+        );
     });
 }
 
