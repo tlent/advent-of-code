a30b6633518a41783d9b8afc2836af2946968737
Thomas Lent <tlent@pm.me>
2023-04-05 07:39:46 -0400

    Optimize day 11 part two using cycle

--- src/day_11/mod.rs
+++ src/day_11/mod.rs
@@ -1,3 +1,4 @@
+use crate::HashMap;
 use std::cell::RefCell;
 
 pub const INPUT: &str = include_str!("input.txt");
@@ -65,7 +66,7 @@ pub fn part_two(monkeys: &[Monkey]) -> usize {
         .iter()
         .map(|monkey| monkey.test.divisor)
         .product::<usize>();
-    solve(monkeys, 10_000, |item| item % modulo)
+    solve_using_cycle(monkeys, 10_000, |item| item % modulo)
 }
 
 fn solve<F>(monkeys: &[Monkey], rounds: usize, after_operation: F) -> usize
@@ -77,7 +78,69 @@ where
         .map(|monkey| RefCell::new(monkey.starting_items.clone()))
         .collect::<Vec<_>>();
     let mut monkey_counts = vec![0; monkeys.len()];
-    for _ in 0..rounds {
+    for _round in 0..rounds {
+        for (i, monkey) in monkeys.iter().enumerate() {
+            let mut items = monkey_items[i].borrow_mut();
+            monkey_counts[i] += items.len();
+            let Test {
+                divisor,
+                true_destination,
+                false_destination,
+            } = monkey.test;
+            for item in items.drain(..) {
+                let result = after_operation(match monkey.operation {
+                    Operation::Add(n) => item + n,
+                    Operation::Multiply(n) => item * n,
+                    Operation::Square => item * item,
+                });
+                let destination = if result % divisor == 0 {
+                    true_destination
+                } else {
+                    false_destination
+                };
+                monkey_items[destination].borrow_mut().push(result);
+            }
+        }
+    }
+    monkey_counts.sort_unstable();
+    monkey_counts.into_iter().rev().take(2).product()
+}
+
+fn solve_using_cycle<F>(monkeys: &[Monkey], rounds: usize, after_operation: F) -> usize
+where
+    F: Fn(usize) -> usize,
+{
+    let monkey_items = monkeys
+        .iter()
+        .map(|monkey| RefCell::new(monkey.starting_items.clone()))
+        .collect::<Vec<_>>();
+    let mut monkey_counts = vec![0; monkeys.len()];
+    let mut seen = HashMap::default();
+    let mut previous_monkey_counts = vec![];
+    for round in 0..rounds {
+        previous_monkey_counts.push(monkey_counts.clone());
+        let inner_items = monkey_items
+            .clone()
+            .into_iter()
+            .map(|i| i.into_inner())
+            .collect::<Vec<_>>();
+        if let Some(&cycle_start) = seen.get(&inner_items) {
+            let cycle_len = round - cycle_start;
+            let remaining_rounds = rounds - round;
+            let remaining_cycles = remaining_rounds / cycle_len;
+            let remainder = remaining_rounds % cycle_len;
+            let cycle_counts = &previous_monkey_counts[cycle_start..];
+            let cycle_start_counts = &cycle_counts[0];
+            let cycle_end_counts = cycle_counts.last().unwrap();
+            let remainder_counts = &cycle_counts[remainder];
+            for (i, count) in monkey_counts.iter_mut().enumerate() {
+                let cycle_increment = cycle_end_counts[i] - cycle_start_counts[i];
+                let remainder_increment = remainder_counts[i] - cycle_start_counts[i];
+                *count += cycle_increment * remaining_cycles + remainder_increment;
+            }
+            break;
+        }
+        seen.insert(inner_items, round);
         for (i, monkey) in monkeys.iter().enumerate() {
             let mut items = monkey_items[i].borrow_mut();
             monkey_counts[i] += items.len();
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::missing_panics_doc)]
 
-use rustc_hash::FxHashSet as HashSet;
+use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 
 pub mod day_01;
 pub mod day_02;
