d9b8a9a08dfd25c596cc0f73c1de1c36e9bf5e48
Thomas Lent <tlent@pm.me>
2023-04-05 19:43:55 -0400

    Refactor

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -2,22 +2,21 @@ use std::collections::VecDeque;
 
 pub const INPUT: &str = include_str!("input.txt");
 
-pub struct Grid {
-    size: usize,
+pub struct HeightMap {
+    width: usize,
+    height: usize,
     heights: Vec<u8>,
-    initial_position: Position,
-    target_position: Position,
+    initial_position: usize,
+    target_position: usize,
 }
 
-impl Grid {
+impl HeightMap {
     fn from_input(input: &str) -> Self {
-        let size = input.lines().nth(0).unwrap().len();
-        let bytes = input
-            .lines()
-            .flat_map(|line| line.bytes())
-            .collect::<Vec<_>>();
-        let initial_position = to_position(bytes.iter().position(|&b| b == b'S').unwrap(), size);
-        let target_position = to_position(bytes.iter().position(|&b| b == b'E').unwrap(), size);
+        let width = input.lines().next().unwrap().len();
+        let height = input.lines().count();
+        let bytes = input.lines().flat_map(str::bytes).collect::<Vec<_>>();
+        let initial_position = bytes.iter().position(|&b| b == b'S').unwrap();
+        let target_position = bytes.iter().position(|&b| b == b'E').unwrap();
         let heights = bytes
             .into_iter()
             .map(|b| match b {
@@ -26,101 +25,72 @@ impl Grid {
                 b if b.is_ascii_lowercase() => b - b'a',
                 _ => panic!("invalid height {b}"),
             })
-            .collect();
-        Grid {
-            size,
+            .collect::<Vec<_>>();
+        HeightMap {
+            width,
+            height,
             heights,
             initial_position,
             target_position,
         }
     }
-
-    fn get(&self, position: Position) -> Option<u8> {
-        if position.x >= self.size {
-            return None;
-        }
-        self.heights.get(to_index(position, self.size)).copied()
-    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Position {
-    x: usize,
-    y: usize,
+pub fn parse_input(input: &str) -> HeightMap {
+    HeightMap::from_input(input)
 }
 
-impl Position {
-    fn neighbors(&self) -> impl Iterator<Item = Position> {
-        let x = self.x;
-        let y = self.y;
-        vec![
-            x.checked_sub(1).map(|x| Position { x, y }),
-            Some(Position { x: x + 1, y }),
-            y.checked_sub(1).map(|y| Position { x, y }),
-            Some(Position { x, y: y + 1 }),
-        ]
-        .into_iter()
-        .filter_map(|option| option)
-    }
-}
-
-pub fn parse_input(input: &str) -> Grid {
-    Grid::from_input(input)
-}
-
-pub fn part_one(grid: &Grid) -> usize {
+pub fn part_one(grid: &HeightMap) -> usize {
     find_shortest_path_len(grid, [grid.initial_position]).unwrap()
 }
 
-pub fn part_two(grid: &Grid) -> usize {
-    let starting_positions = grid.heights.iter().enumerate().filter_map(|(i, &h)| {
-        if h == 0 {
-            Some(to_position(i, grid.size))
-        } else {
-            None
-        }
-    });
+pub fn part_two(grid: &HeightMap) -> usize {
+    let starting_positions =
+        grid.heights
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &h)| if h == 0 { Some(i) } else { None });
     find_shortest_path_len(grid, starting_positions).unwrap()
 }
 
-fn find_shortest_path_len<I>(grid: &Grid, starting_positions: I) -> Option<usize>
+fn find_shortest_path_len<I>(grid: &HeightMap, starting_positions: I) -> Option<usize>
 where
-    I: IntoIterator<Item = Position>,
+    I: IntoIterator<Item = usize>,
 {
     let mut queue = starting_positions
         .into_iter()
         .map(|p| (p, 0))
         .collect::<VecDeque<_>>();
-    let mut seen = vec![false; grid.size * grid.size];
+    let mut seen = vec![false; grid.width * grid.height];
     while let Some((position, steps)) = queue.pop_front() {
         if position == grid.target_position {
             return Some(steps);
         }
-        let seen = &mut seen[to_index(position, grid.size)];
+        let seen = &mut seen[position];
         if *seen {
             continue;
         }
         *seen = true;
-        let height = grid.get(position).unwrap();
-        let valid_neighbors = position.neighbors().filter_map(|n| {
-            grid.get(n)
-                .filter(|&h| h <= height + 1)
-                .map(|_| (n, steps + 1))
+        let x = position % grid.width;
+        let y = position / grid.width;
+        let adjacent_positions = [
+            x.checked_sub(1).map(|x| x + y * grid.width),
+            x.checked_add(1)
+                .filter(|&x| x < grid.width)
+                .map(|x| x + y * grid.width),
+            y.checked_sub(1).map(|y| x + y * grid.width),
+            y.checked_add(1)
+                .filter(|&y| y < grid.height)
+                .map(|y| x + y * grid.width),
+        ]
+        .into_iter()
+        .filter_map(|p| {
+            p.filter(|&p| grid.heights[p] <= grid.heights[position] + 1)
+                .map(|p| (p, steps + 1))
         });
-        queue.extend(valid_neighbors);
+        queue.extend(adjacent_positions);
     }
     None
-}
-
-fn to_position(i: usize, size: usize) -> Position {
-    Position {
-        x: i % size,
-        y: i / size,
-    }
-}
-
-fn to_index(position: Position, size: usize) -> usize {
-    position.y * size + position.x
 }
 
 #[cfg(test)]
