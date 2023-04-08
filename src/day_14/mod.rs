80fc006220e2c6e9eddfce3c2378936af0c0298e
Thomas Lent <tlent@pm.me>
2023-04-08 09:50:51 -0400

    Reorganize

--- src/day_14/mod.rs
+++ src/day_14/mod.rs
@@ -19,6 +19,7 @@ pub struct World {
     map: HashMap<Point, Material>,
     x_bounds: RangeInclusive<usize>,
     y_bounds: RangeInclusive<usize>,
+    settled_sand_unit_count: usize,
 }
 
 impl World {
@@ -36,17 +37,17 @@ impl World {
                 .collect();
             for (&(left_x, left_y), &(right_x, right_y)) in points.iter().zip(points.iter().skip(1))
             {
-                if left_x != right_x {
-                    let start = cmp::min(left_x, right_x);
-                    let end = cmp::max(left_x, right_x);
-                    for x in start..=end {
-                        map.insert((x, left_y), Material::Rock);
-                    }
-                } else {
+                if left_x == right_x {
                     let start = cmp::min(left_y, right_y);
                     let end = cmp::max(left_y, right_y);
                     for y in start..=end {
                         map.insert((left_x, y), Material::Rock);
+                    }
+                } else {
+                    let start = cmp::min(left_x, right_x);
+                    let end = cmp::max(left_x, right_x);
+                    for x in start..=end {
+                        map.insert((x, left_y), Material::Rock);
                     }
                 }
             }
@@ -59,6 +60,31 @@ impl World {
             map,
             x_bounds: min_x..=max_x,
             y_bounds: min_y..=max_y,
+            settled_sand_unit_count: 0,
+        }
+    }
+
+    fn simulate_sand_falling<F1, F2>(&mut self, is_valid_position: F1, should_stop: F2)
+    where
+        F1: Fn(&World, Point) -> bool,
+        F2: Fn(&World, Point) -> bool,
+    {
+        loop {
+            let mut position = Point::default();
+            let mut next_position = Some(SPAWN_POINT);
+            while let Some(next) = next_position {
+                position = next;
+                if should_stop(self, position) {
+                    return;
+                }
+                let (x, y) = position;
+                let candidate_positions = [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)];
+                next_position = candidate_positions
+                    .into_iter()
+                    .find(|&p| is_valid_position(self, p));
+            }
+            self.map.insert(position, Material::Sand);
+            self.settled_sand_unit_count += 1;
         }
     }
 }
@@ -68,47 +94,19 @@ pub fn parse_input(input: &str) -> World {
 }
 
 pub fn part_one(world: &mut World) -> usize {
-    solve(
-        world,
+    world.simulate_sand_falling(
         |world, p| !world.map.contains_key(&p),
         |world, (x, y)| !world.x_bounds.contains(&x) || !world.y_bounds.contains(&y),
-    )
+    );
+    world.settled_sand_unit_count
 }
 
 pub fn part_two(world: &mut World) -> usize {
-    solve(
-        world,
+    world.simulate_sand_falling(
         |world, (x, y)| !world.map.contains_key(&(x, y)) && y < *world.y_bounds.end() + 2,
         |world, _| world.map.contains_key(&SPAWN_POINT),
-    )
-}
-
-fn solve<F1, F2>(world: &mut World, is_valid_position: F1, should_return: F2) -> usize
-where
-    F1: Fn(&World, Point) -> bool,
-    F2: Fn(&World, Point) -> bool,
-{
-    let mut settled_sand_unit_count = 0;
-    loop {
-        let (mut x, mut y) = SPAWN_POINT;
-        loop {
-            if should_return(&world, (x, y)) {
-                return settled_sand_unit_count;
-            }
-            let candidate_positions = [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)];
-            let next_position = candidate_positions
-                .into_iter()
-                .find(|&p| is_valid_position(&world, p));
-            match next_position {
-                Some(p) => (x, y) = p,
-                None => {
-                    world.map.insert((x, y), Material::Sand);
-                    settled_sand_unit_count += 1;
-                    break;
-                }
-            }
-        }
-    }
+    );
+    world.settled_sand_unit_count
 }
 
 #[cfg(test)]
