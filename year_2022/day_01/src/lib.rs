a0d47005bc6df93afb521e8812b84260c0503374
Thomas Lent <tlent@pm.me>
2023-04-12 01:15:20 -0400

    Simplify day 1

--- year_2022/day_01/src/lib.rs
+++ year_2022/day_01/src/lib.rs
@@ -1,5 +1,21 @@
 pub const INPUT: &str = include_str!("../input.txt");
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn parse_input(input: &str) -> Result<Vec<u32>> {
+    let mut sums = vec![];
+    let mut sum = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            sums.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>()?;
+        }
+    }
+    Ok(sums)
+}
+
 pub fn solve(mut group_sums: Vec<u32>) -> (u32, u32) {
     group_sums.sort_unstable();
     (
@@ -8,43 +24,15 @@ pub fn solve(mut group_sums: Vec<u32>) -> (u32, u32) {
     )
 }
 
-pub mod parser {
-    use anyhow::{anyhow, Result};
-    use nom::{
-        character::complete::{line_ending, u32},
-        multi::{fold_many1, separated_list1},
-        sequence::terminated,
-        Finish, IResult,
-    };
-
-    pub fn parse(input: &str) -> Result<Vec<u32>> {
-        let (rest, number_group_sums) = group_sums(input)
-            .finish()
-            .map_err(|err| anyhow!(err.to_string()))?;
-        if !rest.is_empty() {
-            return Err(anyhow!("Unparsed input: {}", rest));
-        }
-        Ok(number_group_sums)
-    }
-
-    fn group_sum(input: &str) -> IResult<&str, u32> {
-        fold_many1(terminated(u32, line_ending), || 0, |acc, item| acc + item)(input)
-    }
-
-    fn group_sums(input: &str) -> IResult<&str, Vec<u32>> {
-        separated_list1(line_ending, group_sum)(input)
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_solve() {
-        let group_sums = parser::parse(INPUT).unwrap();
+        let group_sums = parse_input(INPUT).unwrap();
         let (part_one, part_two) = solve(group_sums);
-        assert_eq!(part_one, 70509);
+        assert_eq!(part_one, 70_509);
         assert_eq!(part_two, 208_567);
     }
 }
--- year_2022/day_01/src/main.rs
+++ year_2022/day_01/src/main.rs
@@ -1,7 +1,7 @@
 use day_01::{self, INPUT};
 
 fn main() {
-    let group_sums = day_01::parser::parse(INPUT).unwrap();
+    let group_sums = day_01::parse_input(INPUT).unwrap();
     let (part_one, part_two) = day_01::solve(group_sums);
     println!("{part_one}\n{part_two}");
 }
