24c24a9634e97cdf4d2c5976f3ae2fb1bda19fba
Thomas Lent <tlent@pm.me>
2023-04-27 21:27:06 -0400

    Use Operation enum

--- year_2022/day_21/src/lib.rs
+++ year_2022/day_21/src/lib.rs
@@ -4,7 +4,34 @@ pub const INPUT: &str = include_str!("../input.txt");
 
 pub enum Value {
     Number(i64),
-    BinaryOp(String, Box<dyn Fn(i64, i64) -> i64>, String),
+    BinaryOp(String, Operation, String),
+}
+
+pub enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Operation {
+    fn inverse(&self) -> Self {
+        match self {
+            Self::Add => Self::Sub,
+            Self::Sub => Self::Add,
+            Self::Mul => Self::Div,
+            Self::Div => Self::Mul,
+        }
+    }
+
+    fn apply(&self, left: i64, right: i64) -> i64 {
+        match self {
+            Self::Add => left + right,
+            Self::Sub => left - right,
+            Self::Mul => left * right,
+            Self::Div => left / right,
+        }
+    }
 }
 
 type Monkeys = HashMap<String, Value>;
@@ -21,11 +48,11 @@ pub fn parse_input(input: &str) -> Monkeys {
             let mut parts = value_str.split_whitespace();
             let left = parts.next().unwrap().to_owned();
             let op_str = parts.next().unwrap();
-            let op: Box<dyn Fn(i64, i64) -> i64> = match op_str {
-                "+" => Box::new(|a, b| a + b),
-                "-" => Box::new(|a, b| a - b),
-                "*" => Box::new(|a, b| a * b),
-                "/" => Box::new(|a, b| a / b),
+            let op = match op_str {
+                "+" => Operation::Add,
+                "-" => Operation::Sub,
+                "*" => Operation::Mul,
+                "/" => Operation::Div,
                 _ => panic!("Unknown operator {}", op_str),
             };
             let right = parts.next().unwrap().to_owned();
@@ -49,7 +76,7 @@ fn find_value(monkeys: &Monkeys, name: &str) -> i64 {
         Value::BinaryOp(left, op, right) => {
             let left_value = find_value(monkeys, left);
             let right_value = find_value(monkeys, right);
-            op(left_value, right_value)
+            op.apply(left_value, right_value)
         }
     }
 }
