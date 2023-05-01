9bf7bd412bfaf922b4d67916169b6788feb06c9a
Thomas Lent <tlent@pm.me>
2023-05-01 19:05:37 -0400

    Create map as a graph

--- year_2022/day_22/src/lib.rs
+++ year_2022/day_22/src/lib.rs
@@ -1,21 +1,5 @@
 pub const INPUT: &str = include_str!("../input.txt");
 
-pub struct Map(Vec<Vec<Tile>>);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Tile {
-    None,
-    Open,
-    Wall,
-}
-
-#[derive(Debug)]
-pub enum PathStep {
-    Forward(usize),
-    Left,
-    Right,
-}
-
 pub fn parse_input(input: &str) -> (Map, Vec<PathStep>) {
     let (map_str, mut path_str) = input.split_once("\n\n").unwrap();
     let tiles = map_str
@@ -80,79 +64,172 @@ pub fn part_two(map: &Map, path: &[PathStep]) -> i32 {
     todo!()
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Tile {
+    Open,
+    Wall,
+}
+
+#[derive(Debug)]
+pub enum PathStep {
+    Forward(usize),
+    Left,
+    Right,
+}
+
+pub struct Map(Vec<MapTile>);
+
+impl Map {
+    fn from_input(input: &str) -> Self {
+        let lines = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let mut tiles: Vec<MapTile> = vec![];
+        let mut column_prevs: Vec<Option<usize>> = vec![];
+        let mut column_segment_starts: Vec<Option<usize>> = vec![];
+        for (i, line) in lines.iter().enumerate() {
+            if line.len() > column_prevs.len() {
+                column_prevs.resize_with(line.len(), Default::default);
+                column_segment_starts.resize_with(line.len(), Default::default);
+            } else if line.len() < column_prevs.len() {
+                let iter = column_segment_starts[line.len()..]
+                    .iter_mut()
+                    .zip(column_prevs[line.len()..].iter_mut());
+                for (start_mut, prev_mut) in iter {
+                    if let Some(start) = start_mut.take() {
+                        let prev = prev_mut.take().unwrap();
+                        tiles[start].next_up = prev;
+                        tiles[prev].next_down = start;
+                    }
+                }
+            }
+            let mut row_prev: Option<usize> = None;
+            let mut row_segment_start: Option<usize> = None;
+            for (j, &c) in line.iter().enumerate() {
+                let column_prev = &mut column_prevs[j];
+                let column_segment_start = &mut column_segment_starts[j];
+                if c == ' ' {
+                    if let Some(start) = row_segment_start.take() {
+                        let prev = row_prev.take().unwrap();
+                        tiles[start].next_left = prev;
+                        tiles[prev].next_right = start;
+                    }
+                    if let Some(start) = column_segment_start.take() {
+                        let prev = column_prev.take().unwrap();
+                        tiles[start].next_up = prev;
+                        tiles[prev].next_down = start;
+                    }
+                    continue;
+                }
+                let tile = match c {
+                    '.' => Tile::Open,
+                    '#' => Tile::Wall,
+                    _ => panic!("invalid map char {c}"),
+                };
+                let map_tile = MapTile {
+                    position: (j + 1, i + 1),
+                    tile,
+                    next_left: row_prev.unwrap_or_default(),
+                    next_up: column_prev.unwrap_or_default(),
+                    next_right: Default::default(),
+                    next_down: Default::default(),
+                };
+                let index = tiles.len();
+                tiles.push(map_tile);
+                if let Some(p) = row_prev {
+                    tiles[p].next_right = index;
+                }
+                if let Some(p) = *column_prev {
+                    tiles[p].next_down = index;
+                }
+                row_prev.insert(index);
+                row_segment_start.get_or_insert(index);
+                column_prev.insert(index);
+                column_segment_start.get_or_insert(index);
+            }
+            let start = row_segment_start.unwrap();
+            let prev = row_prev.unwrap();
+            tiles[start].next_left = prev;
+            tiles[prev].next_right = start;
+        }
+        for (start, prev) in column_segment_starts.into_iter().zip(column_prevs) {
+            if let Some(start) = start {
+                let prev = prev.unwrap();
+                tiles[start].next_up = prev;
+                tiles[prev].next_down = start;
+            }
+        }
+
+        Self(tiles)
+    }
+
+    fn cursor(&self) -> MapCursor {
+        MapCursor {
+            map: self,
+            current_tile: &self.0[0],
+        }
+    }
+}
+
+pub struct MapTile {
+    position: (usize, usize),
+    tile: Tile,
+    next_up: usize,
+    next_left: usize,
+    next_right: usize,
+    next_down: usize,
+}
+
+pub struct MapCursor<'a> {
+    map: &'a Map,
+    current_tile: &'a MapTile,
+}
+
+impl<'a> MapCursor<'a> {
+    fn position(&self) -> (usize, usize) {
+        self.current_tile.position
+    }
+
+    fn tile(&self) -> Tile {
+        self.current_tile.tile
+    }
+
+    fn up(&self) -> Self {
+        Self {
+            map: self.map,
+            current_tile: &self.map.0[self.current_tile.next_up],
+        }
+    }
+
+    fn left(&self) -> Self {
+        Self {
+            map: self.map,
+            current_tile: &self.map.0[self.current_tile.next_left],
+        }
+    }
+
+    fn right(&self) -> Self {
+        Self {
+            map: self.map,
+            current_tile: &self.map.0[self.current_tile.next_right],
+        }
+    }
+
+    fn down(&self) -> Self {
+        Self {
+            map: self.map,
+            current_tile: &self.map.0[self.current_tile.next_down],
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Direction {
     Up,
     Left,
     Right,
     Down,
-}
-
-impl Map {
-    fn first_open_tile_position(&self, row: usize) -> usize {
-        self.0[row - 1]
-            .iter()
-            .position(|&t| t == Tile::Open)
-            .unwrap()
-            + 1
-    }
-
-    fn find_new_position(
-        &self,
-        (mut x, mut y): (i32, i32),
-        facing: Direction,
-        steps: usize,
-    ) -> (i32, i32) {
-        let rows = &self.0;
-        let (dx, dy) = match facing {
-            Direction::Up => (0, -1),
-            Direction::Left => (-1, 0),
-            Direction::Right => (1, 0),
-            Direction::Down => (0, 1),
-        };
-        for _ in 0..steps {
-            let next_x = x + dx;
-            let next_y = y + dy;
-            let next_tile = if self.in_bounds((next_x, next_y)) {
-                rows[next_y as usize][next_x as usize]
-            } else {
-                Tile::None
-            };
-            dbg!((next_x, next_y));
-            match next_tile {
-                Tile::Open => {
-                    x = next_x;
-                    y = next_y;
-                }
-                Tile::Wall => break,
-                Tile::None => {
-                    // TODO: Move this outside of match, only reach match with correct next values
-                    let (dx, dy) = match facing.turn_around() {
-                        Direction::Up => (0, -1),
-                        Direction::Left => (-1, 0),
-                        Direction::Right => (1, 0),
-                        Direction::Down => (0, 1),
-                    };
-                    let mut next_x = x + dx;
-                    let mut next_y = y + dy;
-                    while self.in_bounds((next_x, next_y))
-                        && rows[next_y as usize][next_x as usize] != Tile::None
-                    {
-                        x = next_x;
-                        y = next_y;
-                        next_x = x + dx;
-                        next_y = y + dy;
-                    }
-                }
-            }
-        }
-        (x, y)
-    }
-
-    fn in_bounds(&self, (x, y): (i32, i32)) -> bool {
-        let rows = &self.0;
-        (0..rows.len() as i32).contains(&y) && (0..rows[y as usize].len() as i32).contains(&x)
-    }
 }
 
 impl Direction {
@@ -171,15 +248,6 @@ impl Direction {
             Self::Left => Self::Up,
             Self::Right => Self::Down,
             Self::Down => Self::Left,
-        }
-    }
-
-    fn turn_around(&self) -> Self {
-        match self {
-            Self::Up => Self::Down,
-            Self::Left => Self::Right,
-            Self::Right => Self::Left,
-            Self::Down => Self::Up,
         }
     }
 }
