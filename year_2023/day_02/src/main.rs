6c311e75cecdfbcbd00167a39aa2b0954566cf49
Thomas Lent <tlent@pm.me>
2023-12-02 09:58:10 -0500

    Day 2

---
+++ year_2023/day_02/src/main.rs
@@ -0,0 +1,24 @@
+use year_2023_day_02::INPUT;
+use std::env;
+
+fn main() {
+    let parse_result = year_2023_day_02::parse_input(INPUT);
+    match env::args().nth(1).as_deref() {
+        Some("all") => {
+            let part_one = year_2023_day_02::part_one(&parse_result);
+            println!("{part_one}");
+            let part_two = year_2023_day_02::part_two(&parse_result);
+            println!("{part_two}");
+        }
+        Some("parse") => {}
+        Some("one") => {
+            let part_one = year_2023_day_02::part_one(&parse_result);
+            println!("{part_one}");
+        }
+        Some("two") => {
+            let part_two = year_2023_day_02::part_two(&parse_result);
+            println!("{part_two}");
+        }
+        _ => println!("Invalid argument: must be one of all, parse, one, or two"),
+    }
+}
